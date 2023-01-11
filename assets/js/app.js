27947bbb256736d8de4d7a7c88abb90c392ced34
fauzan-radji <triputrafauzan@gmail.com>
2023-01-12 01:01:55 +0800

    aplot file?

--- assets/js/app.js
+++ assets/js/app.js
@@ -17,3 +17,13 @@ if (error) {
   alert(error);
   sessionStorage.setItem("error", "");
 }
+
+function getFile(base64) {
+  // const base64 = sessionStorage.getItem(id);
+
+  const base64_arr = base64.split(",");
+  const img_format = base64_arr[0].split(";")[1];
+  const img_content = base64_arr[1];
+
+  return new File([img_content], "SERTIFIKAT", { type: img_format });
+}
